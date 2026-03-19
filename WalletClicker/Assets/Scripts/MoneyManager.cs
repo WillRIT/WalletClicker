243e7717816bb9f9191fbb708142aab42a538d97
Kozkoz82 <stc3552@g.rit.edu>
2026-03-19 15:18:10 -0400

    Worked on MoneyManager script (still doesn't work yet)

--- WalletClicker/Assets/Scripts/MoneyManager.cs
+++ WalletClicker/Assets/Scripts/MoneyManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class MoneyManager : MonoBehaviour
 {
+    public TextMeshProUGUI moneyText;
+    uint money;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        money = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        money += 0.5;
+        moneyText.text = money;
     }
 }
