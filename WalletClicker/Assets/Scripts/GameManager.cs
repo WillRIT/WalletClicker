2a646ea040457dd7add36db4b57c02d9cd04b541
Kozkoz82 <stc3552@g.rit.edu>
2026-03-24 14:42:35 -0400

    Money works with UI now

--- WalletClicker/Assets/Scripts/GameManager.cs
+++ WalletClicker/Assets/Scripts/GameManager.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public float money = 0.0f;
+    public TMP_Text moneyText;
     public GameObject wallet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moneyText.text = money.ToString() + "¥";
     }
 }
